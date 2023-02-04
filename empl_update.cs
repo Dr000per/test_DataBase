debdb223e5e48abd0894d0ad2ccdcca1a25f017c
German Shelyakin <wf.pro03@mail.ru>
2023-02-04 09:53:20 +0300

    тне ласт

--- empl_update.cs
+++ empl_update.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace test_DataBase
 {
@@ -35,7 +36,7 @@ namespace test_DataBase
             }
             reader.Close();
 
-            string querystring1 = "select Employee.email from Employee;";
+            string querystring1 = "select Employee.email from Employee where id != 1;";
             SqlCommand command1 = new SqlCommand(querystring1, db_Connection.GetConnection());
             SqlDataReader reader1 = command1.ExecuteReader();
             while (reader1.Read())
@@ -56,20 +57,71 @@ namespace test_DataBase
             string empl_post = comboBox_post.Text;
             string empl_email_new= textBox_email.Text;
             string empl_phone = textBox_phone.Text;
+            int post_id = 0;
+
             try
             {
-                if(checkBox_post.Checked)
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                string querystring1 = $"select id from post where name = '{empl_post}';";
+                SqlCommand command1 = new SqlCommand(querystring1, db_Connection.GetConnection());
+                adapter.SelectCommand = command1;                                                                               // Поиск ID по Post.name
+                db_Connection.openConnection();
+                SqlDataReader reader = command1.ExecuteReader();
+                while (reader.Read())
                 {
-                    string querystring = $"update Employee\r\nset Post = '{empl_post}', Email = Email, Phone = Phone\r\nwhere Email = '{empl_email_now}';";
-                    SqlCommand command = new SqlCommand(querystring, db_Connection.GetConnection());
-                }
-                else if (checkBox_email.Checked) {
 
+                    post_id = int.Parse(reader[0].ToString());
+                    MessageBox.Show(post_id.ToString());
+                }
+                reader.Close();
+                if (checkBox_post.Checked == true && checkBox_phone.Checked == false && checkBox_email.Checked == false)
+                {
+                    string querystring = $"update Employee\r\nset id_post = '{post_id}', Email = Email, Phone = Phone\r\nwhere Email = '{empl_email_now}';";
+                    SqlCommand command = new SqlCommand(querystring, db_Connection.GetConnection());
+
+                    MessageBox.Show("Вы успешно сменили должность сотрудника!", "Оповещение");
+                }
+                else if (checkBox_email.Checked && !checkBox_phone.Checked && !checkBox_post.Checked)
+                {
+                    string querystring = $"update Employee\r\nset id_post = id_post, Email = '{empl_email_new}', Phone = Phone\r\nwhere Email = '{empl_email_now}';";
+                    SqlCommand command = new SqlCommand(querystring, db_Connection.GetConnection());
+
+                    MessageBox.Show("Вы успешно сменили Email сотрудника!", "Оповещение");
+                }
+                else if (checkBox_phone.Checked && !checkBox_post.Checked && !checkBox_email.Checked)
+                {
+                    string querystring = $"update Employee\r\nset id_post = id_post, Email = Email, Phone = '{empl_phone}'\r\nwhere Email = '{empl_email_now}';";
+                    SqlCommand command = new SqlCommand(querystring, db_Connection.GetConnection());
+
+                    MessageBox.Show("Вы успешно сменили телефон сотрудника!", "Оповещение");
+                }
+                else if (checkBox_post.Checked == true && checkBox_email.Checked == true && checkBox_phone.Checked == false)
+                {
+                    string querystring = $"update Employee set id_post = '{post_id}', Email = '{empl_email_new}', Phone = Phone where Email = '{empl_email_now}' and employee.id_post = post.id;";
+                    SqlCommand command = new SqlCommand(querystring, db_Connection.GetConnection());
+                    MessageBox.Show(empl_email_now.ToString(), "Сейчас email");
+                    MessageBox.Show(empl_email_new.ToString(), "Теперь email");
+                    MessageBox.Show(post_id.ToString(), "Должность");
+                    MessageBox.Show("Вы успешно сменили должность и Email сотрудника!", "Оповещение");
+                }
+                else if (checkBox_phone.Checked && checkBox_email.Checked && !checkBox_post.Checked)
+                {
+                    string querystring = $"update Employee\r\nset id_post = id_post, Email = '{empl_email_new}', Phone = '{empl_phone}'\r\nwhere Email = '{empl_email_now}';";
+                    SqlCommand command = new SqlCommand(querystring, db_Connection.GetConnection());
+
+                    MessageBox.Show("Вы успешно сменили телефон и Email сотрудника!", "Оповещение");
+                }
+                else if (checkBox_phone.Checked && checkBox_post.Checked && !checkBox_email.Checked)
+                {
+                    string querystring = $"update Employee\r\nset id_post = '{post_id}', Email = Email, Phone = '{empl_phone}'\r\nwhere Email = '{empl_email_now}';";
+                    SqlCommand command = new SqlCommand(querystring, db_Connection.GetConnection());
+
+                    MessageBox.Show("Вы успешно сменили должность и телефон сотрудника!", "Оповещение");
                 }
             }
             catch(Exception ex)
             {
-
+                MessageBox.Show("Выберите пункты, которые хотите обновить.", "Оповещение");
             }
         }
     }
